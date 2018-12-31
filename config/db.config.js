5ef935d5fc1fa0ebcdbf31b89b937fe104f8111a
Muhammad Ameer Hamza <ameer.hamza@logicps.com>
2018-12-31 19:39:44 +0500

    git ignore

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    secret : 'LogicPoweredSolutions',
-    url: 'mongodb://jobportal:jobportal@cluster0-shard-00-00-airuw.mongodb.net:27017,cluster0-shard-00-01-airuw.mongodb.net:27017,cluster0-shard-00-02-airuw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
-}
-
-
