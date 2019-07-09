408d3fec7143a43d431ee49b0019f6aa36fce803
tahir-shahzad <43000475+tahir-shahzad@users.noreply.github.com>
2019-07-09 16:45:28 +0500

    delete service of subadmin

--- app/models/subadmin.model.js
+++ app/models/subadmin.model.js
@@ -9,14 +9,14 @@ const SubAdminSchema = mongoose.Schema({
     password: String,
     username: {
         type: String,
-        unique: true
+        // unique: true
     },
 
     email: {
         type: String,
         trim: true,
         lowercase: true,
-        unique: true,
+        // unique: true,
         required: 'Email address is required',
         validate: [validateEmail, 'Please fill a valid email address'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
