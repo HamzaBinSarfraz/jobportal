5503879208d55f0cc3436c937641ccf0429abdf3
tahir-shahzad <43000475+tahir-shahzad@users.noreply.github.com>
2019-07-25 17:50:33 +0500

    url fixes

--- app/controllers/chat.controller.js
+++ app/controllers/chat.controller.js
@@ -9,7 +9,7 @@ exports.sendChatMessages = (req, res) => {
         let type = 'file'
         console.log('yes m here');
         // var name = 'https://job-portal-asad.herokuapp.com/' + req.file.filename;
-        var name = 'http://1d4d2e35.ngrok.io/' + req.file.filename;
+        var name = 'https://b944022d.ngrok.io/' + req.file.filename;
 
         let msg = {
             sender: req.body.sender,
